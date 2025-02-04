2d3e1925c40c8ddaf91745b784338e89916390fd
Shubham <you@example.com>
2025-02-04 13:51:07 +0530

    new change

--- src/main/java/com/sb/main/demo/demo1.java
+++ src/main/java/com/sb/main/demo/demo1.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class demo1 {
 	public String run() {
-		System.out.println("hallo");
+		System.out.println("hallo word");
 		return "hallo2";
 	}
 
